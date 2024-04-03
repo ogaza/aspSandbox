ef54d96f58e0083a6f9b21945cb2bbe0b38452ae
Olaf Gaża <olaf.gaza.fp@gmail.com>
2024-04-03 10:50:38 +0200

    form_validation: comment out js fetch data

--- forms/index.js
+++ forms/index.js
@@ -1,10 +1,10 @@
-async function getData() {
-  const response = await fetch(
-    "http://localhost:9090/regions/api/regionsApi.asp"
-  );
-  const responseJSON = await response.json();
+// async function getData() {
+//   const response = await fetch(
+//     "http://localhost:9090/regions/api/regionsApi.asp"
+//   );
+//   const responseJSON = await response.json();
 
-  console.log(responseJSON);
-}
+//   console.log(responseJSON);
+// }
 
-getData();
+// getData();
