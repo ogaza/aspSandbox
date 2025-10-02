e3ce585cc2f098e056e8ab697cdce038c14bb35d
Olaf Gaza <c-ogaza@xpansiv.com>
2025-10-02 14:13:12 +0200

    zap-testing: modify sender so it adds or updates csfr token param

--- ZapScripts/http_sender.js
+++ ZapScripts/http_sender.js
@@ -7,7 +7,7 @@ var regex = /name\s*=\s*"c16e"\s*value\s*=\s*"([^"]*)"/;
 var csfrTokenValue = "4d90d9e6adfc8f86b06d634fca481d5b";
 
 function sendingRequest(msg, initiator, helper) {
-  print("sending request with the ");
+  print("sending request");
   print("HEADER: ");
   print("---------------------------------------");
   print(msg.getRequestHeader().toString());
@@ -15,43 +15,65 @@ function sendingRequest(msg, initiator, helper) {
   var requestBody = msg.getRequestBody().toString();
 
   var httpMethod = msg.getRequestHeader().getMethod();
-  var contentType = msg.getRequestHeader().getHeader("content-type");
-  var isFormPOST = contentType === "application/x-www-form-urlencoded";
+  var isFormPOST = (httpMethod === "POST");
+  // var contentType = msg.getRequestHeader().getHeader("content-type");
+  // var isFormPOST = (contentType === "application/x-www-form-urlencoded");
 
   var formParam;
   var formParams = msg.getFormParams().toArray();
-  var containsCSFRparam = false;
+  // var containsCSFRparam = false;
 
-  print("---------------------------------------");
-  print("form params: ");
-  print(formParams);
+  // print("---------------------------------------");
+  // print("form params: ");
+  // print(formParams);
 
+  /*
   for (var i = 0; i < formParams.length; i++) {
     formParam = formParams[i];
-
     if (formParam.name === csfrTokenName) {
       containsCSFRparam = true;
       break;
     }
-    //print("name  : " + formParam?.name);
-    //print("value : " + formParam?.value);
   }
+  */
+  if (isFormPOST) {
+    var containsCSFRparam = false;
 
-  if (isFormPOST && !containsCSFRparam) {
     csfrTokenValue = ScriptVars.getGlobalVar("csrf_token.value");
 
-    print("---------------------------------------");
-    print("ADDING CSRF TOKEN");
-    print("csfrTokenValue from global ScriptVars: " + csfrTokenValue);
-    print("---------------------------------------");
+    var i = 0;
+    var params = msg.getFormParams();
+    var iterator = params.iterator();
 
-    var formParams = msg.getFormParams();
+    while (iterator.hasNext() && i < formParams.length) {
+      var param = iterator.next();
 
-    if (requestBody.length !== 0) requestBody += "&";
-    requestBody += csfrTokenName + "=" + encodeURIComponent(csfrTokenValue);
-    msg.setRequestBody(requestBody);
+      if (param.getName().equals(csfrTokenName)) {
+        containsCSFRparam = true;
+        param.setValue(csfrTokenValue);
+      }
+
+      print("param: ");
+      print(param);
+
+      i = i + 1;
+    }
+    msg.setFormParams(params);
+
+    if(!containsCSFRparam) {
+      print("---------------------------------------");
+      print("ADDING CSRF TOKEN");
+      print("csfrTokenValue from global ScriptVars: " + csfrTokenValue);
+      print("---------------------------------------");
+
+      var formParams = msg.getFormParams();
+
+      if (requestBody.length !== 0) requestBody += "&";
+      requestBody += csfrTokenName + "=" + encodeURIComponent(csfrTokenValue);
+      msg.setRequestBody(requestBody);
+    }
+
     msg.getRequestHeader().setContentLength(msg.getRequestBody().length());
-
     /*
     var c16e = new HtmlParameter(
       htmlParamTypeForm,
@@ -79,4 +101,28 @@ function responseReceived(msg, initiator, helper) {
 
     ScriptVars.setGlobalVar("csrf_token.value", csfrToken);
   }
+}
+
+function modifyParams(params) {
+  var iterator = params.iterator();
+  while (iterator.hasNext()) {
+    var param = iterator.next();
+    // Check if the url parameters has the antiCsrfTokenName in it.
+    if (param.getName().equals(antiCsrfTokenName)) {
+      var secureTokenValue = param.getValue();
+      var antiCsrfTokenValue =
+        org.zaproxy.zap.extension.script.ScriptVars.getGlobalVar(
+          "anti.csrf.token.value"
+        );
+      // Check for the value of AntiCsrfTokenName in the existing request with the latest value captured from previous requests.
+      if (
+        antiCsrfTokenValue != null &&
+        !secureTokenValue.equals(antiCsrfTokenValue)
+      ) {
+        param.setValue(antiCsrfTokenValue);
+        break;
+      }
+    }
+  }
+  return params;
 }
