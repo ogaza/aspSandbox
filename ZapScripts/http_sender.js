77a249b8b35a50d7783e6c8e2b54299ac908691b
Olaf Gaza <c-ogaza@xpansiv.com>
2025-10-03 11:27:31 +0200

    ap-testing: sender script which makes sure all POST request have proper csfr token

--- ZapScripts/http_sender.js
+++ ZapScripts/http_sender.js
@@ -4,125 +4,79 @@ var ScriptVars = org.zaproxy.zap.extension.script.ScriptVars;
 
 var csfrTokenName = "c16e";
 var regex = /name\s*=\s*"c16e"\s*value\s*=\s*"([^"]*)"/;
-var csfrTokenValue = "4d90d9e6adfc8f86b06d634fca481d5b";
+var csfrTokenValue; // = "4d90d9e6adfc8f86b06d634fca481d5b";
 
 function sendingRequest(msg, initiator, helper) {
+  var requestHeader = msg.getRequestHeader();
+
   print("sending request");
+  print("---------------------------------------");
   print("HEADER: ");
   print("---------------------------------------");
-  print(msg.getRequestHeader().toString());
+  print(requestHeader.toString());
 
-  var requestBody = msg.getRequestBody().toString();
+  var httpMethod = requestHeader.getMethod();
+  var isFormPOST = httpMethod === "POST";
 
-  var httpMethod = msg.getRequestHeader().getMethod();
-  var isFormPOST = (httpMethod === "POST");
-  // var contentType = msg.getRequestHeader().getHeader("content-type");
-  // var isFormPOST = (contentType === "application/x-www-form-urlencoded");
-
-  var formParam;
-  var formParams = msg.getFormParams().toArray();
-  // var containsCSFRparam = false;
-
-  // print("---------------------------------------");
-  // print("form params: ");
-  // print(formParams);
-
-  /*
-  for (var i = 0; i < formParams.length; i++) {
-    formParam = formParams[i];
-    if (formParam.name === csfrTokenName) {
-      containsCSFRparam = true;
-      break;
-    }
-  }
-  */
   if (isFormPOST) {
-    var containsCSFRparam = false;
+    modifyMsgSoTheFormParamsContainCSFR(msg);
+  }
 
-    csfrTokenValue = ScriptVars.getGlobalVar("csrf_token.value");
+  print("---------------------------------------");
+  print("BODY: ");
+  print("---------------------------------------");
+  print(msg.getRequestBody().toString().replace("&", "\n"));
+  print("=======================================");
+}
 
-    var i = 0;
-    var params = msg.getFormParams();
-    var iterator = params.iterator();
+function responseReceived(msg, initiator, helper) {
+  print("Response received");
+  // print("Status code: " + msg.getResponseHeader().getStatusCode());
+  print("---------------------------------------");
+  print("HEADER: ");
+  print("---------------------------------------");
+  print(msg.getResponseHeader());
+  // print("BODY:")
+  // print(msg.getResponseBody());
 
-    while (iterator.hasNext() && i < formParams.length) {
-      var param = iterator.next();
+  const responseBody = msg.getResponseBody().toString();
+  const match = responseBody.match(regex);
 
-      if (param.getName().equals(csfrTokenName)) {
-        containsCSFRparam = true;
-        param.setValue(csfrTokenValue);
-      }
+  if (match && match.length > 0) {
+    const csfrToken = match[1];
+    print("---------------------------------------");
+    print("csfr token from the response:");
+    print("---------------------------------------");
+    print(csfrToken);
 
-      print("param: ");
-      print(param);
+    ScriptVars.setGlobalVar("csrf_token.value", csfrToken);
+  }
+  print("=======================================");
+}
 
-      i = i + 1;
+function modifyMsgSoTheFormParamsContainCSFR(msg) {
+  var formParams = msg.getFormParams();
+
+  csfrTokenValue = ScriptVars.getGlobalVar("csrf_token.value");
+
+  var containsCSFRparam = false;
+  formParams?.forEach(function (e) {
+    if (e.getName() == csfrTokenName) {
+      e.setValue(csfrTokenValue);
+      containsCSFRparam = true;
     }
-    msg.setFormParams(params);
+  });
 
-    if(!containsCSFRparam) {
-      print("---------------------------------------");
-      print("ADDING CSRF TOKEN");
-      print("csfrTokenValue from global ScriptVars: " + csfrTokenValue);
-      print("---------------------------------------");
-
-      var formParams = msg.getFormParams();
-
-      if (requestBody.length !== 0) requestBody += "&";
-      requestBody += csfrTokenName + "=" + encodeURIComponent(csfrTokenValue);
-      msg.setRequestBody(requestBody);
-    }
-
-    msg.getRequestHeader().setContentLength(msg.getRequestBody().length());
-    /*
+  if (!containsCSFRparam) {
     var c16e = new HtmlParameter(
       htmlParamTypeForm,
       csfrTokenName,
       csfrTokenValue
     );
-    msg.getFormParams().add(c16e);
-    */
+
+    formParams.add(c16e);
   }
-  print("BODY: ");
-  print("---------------------------------------");
-  print(msg.getRequestBody().toString());
 
-  print("=======================================");
-}
-
-function responseReceived(msg, initiator, helper) {
-  const responseBody = msg.getResponseBody().toString();
-  const match = responseBody.match(regex);
-
-  if (match && match.length > 0) {
-    // match[0] is the full match, match[1] is the first captured group
-    const csfrToken = match[1];
-    print("got csfr token from the response:" + csfrToken);
-
-    ScriptVars.setGlobalVar("csrf_token.value", csfrToken);
-  }
-}
-
-function modifyParams(params) {
-  var iterator = params.iterator();
-  while (iterator.hasNext()) {
-    var param = iterator.next();
-    // Check if the url parameters has the antiCsrfTokenName in it.
-    if (param.getName().equals(antiCsrfTokenName)) {
-      var secureTokenValue = param.getValue();
-      var antiCsrfTokenValue =
-        org.zaproxy.zap.extension.script.ScriptVars.getGlobalVar(
-          "anti.csrf.token.value"
-        );
-      // Check for the value of AntiCsrfTokenName in the existing request with the latest value captured from previous requests.
-      if (
-        antiCsrfTokenValue != null &&
-        !secureTokenValue.equals(antiCsrfTokenValue)
-      ) {
-        param.setValue(antiCsrfTokenValue);
-        break;
-      }
-    }
-  }
-  return params;
+  msg.getRequestBody().setFormParams(formParams);
+  msg.getRequestHeader().setContentLength(msg.getRequestBody().length());
 }
